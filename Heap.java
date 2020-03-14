6fe7a914f97e58a36b7b799d7abc42df2e8801db
ayagamalmh98 <48804613+ayagamalmh98@users.noreply.github.com>
2020-03-14 15:46:44 +0200

    Add files via upload

--- Heap.java
+++ Heap.java
@@ -7,135 +7,136 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Heap<T extends Comparable<T>> implements IHeap<T> {
-	private int Length = 0;
-	private List<INode<T>> elements = new ArrayList<>();
+    private int Length = 0;
+    private List<INode<T>> elements = new ArrayList<>();
 
-	@SuppressWarnings("hiding")
-	private class Node<T extends Comparable<T>> implements INode<T> {
-		private int index;
-		private T value;
+    @SuppressWarnings("hiding")
+    private class Node<T extends Comparable<T>> implements INode<T> {
+        private int index;
+        private T value;
 
-		public Node(int index) {
-			this.index = index;
-		}
+        public Node(int index) {
+            this.index = index;
+        }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		public INode<T> getLeftChild() {
-			if (2 * index > Length) {
-				return null;
-			}
-			return (INode<T>) elements.get(2 * index);
-		}
+        @SuppressWarnings("unchecked")
+        @Override
+        public INode<T> getLeftChild() {
+            if (2 * index > Length) {
+                return null;
+            }
+            return (INode<T>) elements.get(2 * index);
+        }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		public INode<T> getRightChild() {
-			if (2 * index + 1 > Length) {
-				return null;
-			}
-			return (INode<T>) elements.get(2 * index + 1);
-		}
+        @SuppressWarnings("unchecked")
+        @Override
+        public INode<T> getRightChild() {
+            if (2 * index + 1 > Length) {
+                return null;
+            }
+            return (INode<T>) elements.get(2 * index + 1);
+        }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		public INode<T> getParent() {
-			if (index / 2 >= Length) {
-				return null;
-			}
-			return (INode<T>) elements.get(index / 2);
-		}
+        @SuppressWarnings("unchecked")
+        @Override
+        public INode<T> getParent() {
+            if (index / 2 >= Length) {
+                return null;
+            }
+            return (INode<T>) elements.get(index / 2);
+        }
 
-		@Override
-		public T getValue() {
-			return value;
-		}
+        @Override
+        public T getValue() {
+            return value;
+        }
 
-		@Override
-		public void setValue(T value) {
-			this.value = value;
+        @Override
+        public void setValue(T value) {
+            this.value = value;
 
-		}
-	}
+        }
+    }
 
-	public INode<T> getRoot() {
-		if (elements.size() == 0) {
-			return null;
-		}
-		return elements.get(1);
-	}
+    public INode<T> getRoot() {
+        if (elements.size() <= 1) {
+            return null;
+        }
+        return elements.get(1);
+    }
 
-	public int size() {
-		return Length;
-	}
+    public int size() {
 
-	void swap(final INode<T> node1, final INode<T> node2) {
-		T temp = (T) node1.getValue();
-		node1.setValue(node2.getValue());
-		node2.setValue(temp);
-	}
+        return Length;
+    }
 
-	public void heapify(INode<T> node) {
+    void swap(final INode<T> node1, final INode<T> node2) {
+        T temp = (T) node1.getValue();
+        node1.setValue(node2.getValue());
+        node2.setValue(temp);
+    }
 
-		if (node != null) {
-			INode<T> left = node.getLeftChild();
-			INode<T> right = node.getRightChild();
-			INode<T> bigger;
-			if (left != null && left.getValue().compareTo(node.getValue()) > 0) {
-				bigger = left;
-			} else {
-				bigger = node;
-			}
-			if (right != null && right.getValue().compareTo(bigger.getValue()) > 0) {
-				bigger = right;
-			}
-			if (bigger != node) {
-				swap(node, bigger);
-				if (node != getRoot())
-					heapify(node.getParent());
-				heapify(bigger);
-			}
-		}
-	}
+    public void heapify(INode<T> node) {
 
-	public T extract() {
-		if (elements.isEmpty()) {
-			return null;
-		}
-		T max = elements.get(1).getValue();
-		elements.get(1).setValue(elements.get(Length).getValue());
-		elements.remove(Length);
-		Length--;
-		if (Length != 0) {
-			heapify(elements.get(1));
-		}
-		return max;
-	}
+        if (node != null) {
+            INode<T> left = node.getLeftChild();
+            INode<T> right = node.getRightChild();
+            INode<T> bigger;
+            if (left != null && left.getValue().compareTo(node.getValue()) > 0) {
+                bigger = left;
+            } else {
+                bigger = node;
+            }
+            if (right != null && right.getValue().compareTo(bigger.getValue()) > 0) {
+                bigger = right;
+            }
+            if (bigger != node) {
+                swap(node, bigger);
+                if (node != getRoot())
+                    heapify(node.getParent());
+                heapify(bigger);
+            }
+        }
+    }
 
-	public void insert(T element) {
-		if (Length == 0) {
-			elements.add(null);
-		}
-		if (element == null) {
-			return;
-		}
-		INode<T> node = new Node<T>(Length + 1);
-		node.setValue(element);
-		elements.add(Length + 1, node);
-		Length++;
-		heapifyUp(node);
-	}
+    public T extract() {
+        if (elements.size() <= 1){
+            return null;
+        }
+        T max = elements.get(1).getValue();
+        elements.get(1).setValue(elements.get(Length).getValue());
+        elements.remove(Length);
+        Length--;
+        if (Length != 0) {
+            heapify(elements.get(1));
+        }
+        return max;
+    }
 
-	private void heapifyUp(INode<T> node) {
-			while (node != getRoot() && node.getParent() != null
-					&& node.getValue().compareTo(node.getParent().getValue()) > 0) {
-				swap(node, node.getParent());
-				node = node.getParent();
-			}
-	}
+    public void insert(T element) {
+        if (Length == 0) {
+            elements.add(null);
+        }
+        if (element == null) {
+            return;
+        }
+        INode<T> node = new Node<T>(Length + 1);
+        node.setValue(element);
+        elements.add(Length + 1, node);
+        Length++;
+        heapifyUp(node);
+    }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void build(Collection unordered) {
+    private void heapifyUp(INode<T> node) {
+        while (node != getRoot() && node.getParent() != null
+                && node.getValue().compareTo(node.getParent().getValue()) > 0) {
+            swap(node, node.getParent());
+            node = node.getParent();
+        }
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void build(Collection unordered) {
 	/*	if (unordered == null) {
 			Length = 0;
 			return;
@@ -155,16 +156,27 @@ public class Heap<T extends Comparable<T>> implements IHeap<T> {
 		for (int i = elements.size() / 2; i > 0; i--) {
 			heapify(elements.get(i));
 		}*/
-		if (unordered == null) {
-			Length = 0;
-			return;
-		}
-		int length = unordered.size();
-		elements = (List<INode<T>>) unordered;
-		for (int i = length / 2; i > 0; i--) {
-			heapify(elements.get(i));
-		}
+        if (unordered == null) {
+            Length = 0;
+            return;
+        }
+        int length = unordered.size();
+        elements = (List<INode<T>>) unordered;
+        for (int i = length / 2; i > 0; i--) {
+            heapify(elements.get(i));
+        }
 
-	}
+    }
+    public IHeap<T> sort(ArrayList<T> unordered) {
+        this.build(unordered);
+
+        for (int i = Length ; i >= 2; i--) {
+            swap(elements.get(1), elements.get(i));
+            Length--;
+            this.heapify(elements.get(0));
+        }
+        Length = unordered.size();
+        return this;
+    }
 
 }
